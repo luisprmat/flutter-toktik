831bd0de9210bf939ddf2809bcd71889e5289da0
Luis Parrado <luisprmat@gmail.com>
2025-04-07 23:15:11 -0500

    Structures, entities + how to add static assets

---
+++ lib/shared/data/local_video_posts.dart
@@ -0,0 +1,56 @@
+// List download from Gist: https://gist.github.com/Klerith/ae81077b881792ece6d8a1edd0e94d2f
+// Videos downloaded from lesson page video: https://cursos.devtalles.com/courses/take/flutter-movil-cero-a-experto/lessons/42683001-estructuras-y-entidades
+// (https://import.cdn.thinkific.com/643563/courses/2177248/videos-230214-085809.zip)
+// it's possible download more videos from: https://www.pexels.com/search/videos/vertical/
+// ** This videos must be downloaded and unzipped in the folder /assets ** (They will not be tracked in GitHub due to its large size.)
+
+List<Map<String, dynamic>> videoPosts = [
+  {
+    'name': 'Subiendo escaleras automáticas',
+    'videoUrl': 'assets/videos/1.mp4',
+    'likes': 23230,
+    'views': 1523,
+  },
+  {
+    'name': 'Planta apreciada por peatones',
+    'videoUrl': 'assets/videos/2.mp4',
+    'likes': 24230,
+    'views': 1343,
+  },
+  {
+    'name': 'Que borroso veo todo!',
+    'videoUrl': 'assets/videos/3.mp4',
+    'likes': 21564320,
+    'views': 123563,
+  },
+  {
+    'name': '¿Esto es trigo? que interesante',
+    'videoUrl': 'assets/videos/4.mp4',
+    'likes': 320,
+    'views': 2300,
+  },
+  {
+    'name': 'El COVID no me afecta',
+    'videoUrl': 'assets/videos/5.mp4',
+    'likes': 3230,
+    'views': 31030,
+  },
+  {
+    'name': 'No quiero ir a trabajar hoy señor Stark',
+    'videoUrl': 'assets/videos/6.mp4',
+    'likes': 10,
+    'views': 330,
+  },
+  {
+    'name': 'Limpiar nunca fue tan divertido',
+    'videoUrl': 'assets/videos/7.mp4',
+    'likes': 1320,
+    'views': 33032,
+  },
+  {
+    'name': '¿Ya llegamos a la India?... umm si',
+    'videoUrl': 'assets/videos/8.mp4',
+    'likes': 342,
+    'views': 3332,
+  },
+];
