0ef61e4f05aeff01dfcfbc11b22f66ed16784cf3
Luis Parrado <luisprmat@gmail.com>
2025-04-09 18:58:31 -0500

    Datasource and Repositories (abstract classes)

---
+++ lib/domain/repositories/video_posts_repository.dart
@@ -0,0 +1,7 @@
+import 'package:toktik/domain/entities/video_post.dart';
+
+abstract class VideoPostsRepository {
+  Future<List<VideoPost>> getFavoriteVideosByUser(String userId);
+
+  Future<List<VideoPost>> getTrendingVideosByPage(int page);
+}
